fced0d1df0240d8608ee20f8fa8ef63d5f2f21b2
赵宇 <yu.zhao@dipath.cn>
2023-11-21 16:48:30 +0800

    基础镜像dockerfile变更

--- cyborg/modules/ai/utils/file.py
+++ cyborg/modules/ai/utils/file.py
@@ -1,7 +1,10 @@
+import logging
 import os
 from io import BytesIO
 
 from cyborg.infra.oss import oss
+
+logger = logging.getLogger(__name__)
 
 ALG_MODEL_PATH = "/data/model/"
 
@@ -9,9 +12,11 @@ ALG_MODEL_PATH = "/data/model/"
 def load_alg_model(file_key) -> BytesIO:
     model_path = os.path.join(ALG_MODEL_PATH, file_key)
     if os.path.exists(model_path):
+        logger.info(f'load model from local file: {model_path}')
         buffer = BytesIO()
         with open(model_path, "rb") as f:
             buffer.write(f.read())
             buffer.seek(0)
         return buffer
+    logger.info(f'load model from oss: {model_path}')
     return oss.get_object_to_io(file_key)
--- deployment/roche/dockerfile/cyborg-roche-her2.Dockerfile
+++ deployment/roche/dockerfile/cyborg-roche-her2.Dockerfile
@@ -1,4 +1,4 @@
-FROM registry.cn-hangzhou.aliyuncs.com/dipath/cyborg:base
+FROM dipath/cyborg:base
 
 WORKDIR /data/www/cyborg
 
@@ -9,8 +9,6 @@ ADD ./requirements.txt ./requirements.txt
 
 RUN pip install -r requirements.txt
 
-RUN mkdir -p /data/model
 RUN mkdir -p /data/download
-ADD /data/model/AI /data/model/AI
 
 RUN python encrypt.py build_ext --inplace
